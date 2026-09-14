c476f9c41a517a35e3bf0b6bc5098350dd876055
ezraidimariam <ezraidimariam@users.noreply.github.com>
2026-09-14 01:23:02 +0100

    style: remove generated build folder and shortcut

--- src/model/Compte.java
+++ src/model/Compte.java
@@ -1,24 +1,24 @@
-package model;
+    package model;
 
-import java.util.HashSet;
+    import java.util.HashSet;
 
-public class Compte {
-    private static int counter = 1001;
-    private String numeroCompte;
-    private double solde;
-    private TypeCompte typeCompte;
-    private HashSet<Transaction> historiqueTransactions;
+    public class Compte {
+        private static int counter = 1001;
+        private String numeroCompte;
+        private double solde;
+        private TypeCompte typeCompte;
+        private HashSet<Transaction> historiqueTransactions;
 
-    public Compte(double solde, TypeCompte typeCompte) {
-        this.numeroCompte = "CB" + counter++;
-        this.solde = solde;
-        this.typeCompte = typeCompte;
-        this.historiqueTransactions = new HashSet<>();
+        public Compte(double solde, TypeCompte typeCompte) {
+            this.numeroCompte = "CB" + counter++;
+            this.solde = solde;
+            this.typeCompte = typeCompte;
+            this.historiqueTransactions = new HashSet<>();
+        }
+
+        public String getNumeroCompte() { return numeroCompte; }
+        public double getSolde() { return solde; }
+        public void setSolde(double solde) { this.solde = solde; }
+        public TypeCompte getTypeCompte() { return typeCompte; }
+        public HashSet<Transaction> getHistoriqueTransactions() { return historiqueTransactions; }
     }
-
-    public String getNumeroCompte() { return numeroCompte; }
-    public double getSolde() { return solde; }
-    public void setSolde(double solde) { this.solde = solde; }
-    public TypeCompte getTypeCompte() { return typeCompte; }
-    public HashSet<Transaction> getHistoriqueTransactions() { return historiqueTransactions; }
-}
